e56214b078e3ef3ca9c7f61c1c5c37fe5b1679c0
Vladyslav <vladoromus@gmail.com>
2025-04-18 14:54:15 +0300

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -4,24 +4,24 @@ from app.main import outdated_products
 
 
 @pytest.fixture()
-def data() -> None:
+def data() -> list:
     return [
-    {
-        "name": "salmon",
-        "expiration_date": datetime.date(2025, 4, 10),
-        "price": 600
-    },
-    {
-        "name": "chicken",
-        "expiration_date": datetime.date(2025, 4, 17),
-        "price": 120
-    },
-    {
-        "name": "duck",
-        "expiration_date": datetime.date(2025, 4, 18),
-        "price": 160
-    }
-]
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2025, 4, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2025, 4, 17),
+            "price": 120,
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2025, 4, 18),
+            "price": 160,
+        },
+    ]
 
 
 def test_outdated_products(data: list) -> None:
