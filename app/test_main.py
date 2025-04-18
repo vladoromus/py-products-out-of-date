fb5ac9805d899871cb3f28a5bcba2cb51146739a
Vladyslav <vladoromus@gmail.com>
2025-04-18 14:51:26 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+import pytest
+import datetime
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def data() -> None:
+    return [
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date(2025, 4, 10),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2025, 4, 17),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2025, 4, 18),
+        "price": 160
+    }
+]
+
+
+def test_outdated_products(data: list) -> None:
+    assert outdated_products(data) == ["salmon", "chicken"]
